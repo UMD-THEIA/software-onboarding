97f32a20ff67d2da1998467c666366bb43bc50d3
mBuschauer <marco@buschauer.us>
2025-10-09 19:02:43 -0400

    fixed windows ssh

--- src/pages/setup_pages/windows.js
+++ src/pages/setup_pages/windows.js
@@ -45,8 +45,18 @@ export default function Windows() {
     </p>
     <CodeBlock language="shell" code={`echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc`} />
 
-    <h3 className='setupText'>Set Up SSH for GitHub</h3>
-    <GithubSSH />
+    <h3 className='setupText'>Setting up Github with an SSH Key</h3>
+    <p className='setupText'>First, generate an SSH Key</p>
+    <CodeBlock language="shell" code={`ssh-keygen -t ed25519 -C "your_email@example.com"`} />
+    <p className='setupText'>When prompted, save the key in ~/.ssh/id_ed25519. You can enter a passphrase or leave it empty.</p>
+   
+    <p className='setupText'>Next, copy your SSH key into Github</p>
+    <CodeBlock language="shell" code={`cat ~/.ssh/id_ed25519.pub`} />
+    <p className='setupText'>Go to <a rel="noreferrer" href="https://github.com/settings/keys" target="_blank">GitHub SSH Keys</a>, click <b>New SSH Key</b>, paste your key, and save.</p>
+    <p className='setupText'>Finally test the connection:</p>
+    <CodeBlock language="shell" code={`ssh -T git@github.com`} />
+    <p className='setupText'>You should expect the following outpute before it exits:</p>
+    <CodeBlock language="shell" code={`Hi github_username! You've successfully authenticated, but GitHub does not provide shell access.`} />
 
     <h3 className='setupText'>Create a Development Directory</h3>
     <p className='setupText'>Inside WSL, create a folder for your project files:</p>
